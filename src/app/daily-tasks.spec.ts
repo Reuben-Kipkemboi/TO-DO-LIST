90c44132a66ae6670b4a1db1221f7fe62c9b0d59
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-06 23:31:54 +0300

    st:task-class - creates a tasks class

---
+++ src/app/daily-tasks.spec.ts
@@ -0,0 +1,7 @@
+import { DailyTasks } from './daily-tasks';
+
+describe('DailyTasks', () => {
+  it('should create an instance', () => {
+    expect(new DailyTasks()).toBeTruthy();
+  });
+});
---
+++ src/app/daily-tasks.ts
@@ -0,0 +1,4 @@
+export class DailyTasks {
+    id: number | undefined; //undefined tells us that the value of id is undefined yet./no absolute value
+    descriptionOfTask: Task | undefined; // the undefined shows tthat the value of task is not assigned
+}
