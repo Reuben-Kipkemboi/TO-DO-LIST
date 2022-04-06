a7f33c307232cf17c8e71bea9c2b013b4db88013
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-06 23:53:20 +0300

    pt:array - fix array display error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,13 @@ export class AppComponent {
    //use of class to create the daily tasks
   tasks:DailyTasks[] =[
     {id:1, descriptionOfTask:'Watch live movie'},
-    {id:1, descriptionOfTask:'Watch Real Madrid vs Chelsea'},
-    {id:1, descriptionOfTask:'Visit Rwanda'},
-    {id:1, descriptionOfTask:'Make an Angular TO do List '},
-    {id:1, descriptionOfTask:'learn form validation'}
-    {id:1, descriptionOfTask:'create and deploy an angular application'}
+    {id:2, descriptionOfTask:'Watch Real Madrid vs Chelsea'},
+    {id:3, descriptionOfTask:'Visit Rwanda'},
+    {id:4, descriptionOfTask:'Make an Angular TO do List '},
+    {id:5, descriptionOfTask:'learn form validation'},
+    {id:6, descriptionOfTask:'create and deploy an angular application'}
 
   ];
 
-  constructor(){
-    this.tasks = ['make money', 'Visit Rwanda']
-  }
+  
 }
